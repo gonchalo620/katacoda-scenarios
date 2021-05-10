19bdbf5260ce2b231341dc239aa3153ce31b7a13
Danilo Chica <39041551+danilochica@users.noreply.github.com>
2021-05-10 07:30:05 -0500

    Update intro-background.sh

--- git/fixing-merge-conflicts/intro-background.sh
+++ git/fixing-merge-conflicts/intro-background.sh
@@ -8,12 +8,12 @@ git checkout "features/merge_$number_random"
 echo 'console.log("Modificado por otro desarrollador");' > $file
 git add $file
 git commit -m "Commit Inicial"
-git push https://tb3yofvnvj2dunk5ty6xlq6574lmk66eqnbfltjazlyjwedix2kq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git "features/merge_$number_random"
+git push https://kyztsenirkho4eehlsxcu6ownvscgzg23rp6dgtrcpwkidwp35xq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git "features/merge_$number_random"
 sleep 3
 git fetch origin
 git checkout remotes/origin/master
 git merge remotes/origin/"features/merge_$number_random"
-git push https://tb3yofvnvj2dunk5ty6xlq6574lmk66eqnbfltjazlyjwedix2kq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git
+git push https://kyztsenirkho4eehlsxcu6ownvscgzg23rp6dgtrcpwkidwp35xq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git
 sleep 3
 git checkout master
 git checkout .
